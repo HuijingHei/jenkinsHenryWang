9c8437e3e9ab63bb4e24bae7f22d4394d2b1f7ff
Huijing Hei <hhei@redhat.com>
2019-04-10 10:29:19 +0800

    debug hyper-v run

---
+++ ProvisionVM/hyperv-run.ps1
@@ -0,0 +1,144 @@
+param (
+    [CmdletBinding()]
+    [String] $action,
+    [String] $nvr,
+    [Switch] $dual,
+    [Switch] $gen2
+)
+$DebugPreference = "Continue"
+Write-Debug "Get action = $action"
+Write-Debug "Get nvr = $nvr"
+Write-Debug "Get dual = $dual"
+Write-Debug "Get gen2 = $gen2"
+
+if (${env:IMAGE}) {
+    $tmp = ${env:IMAGE}.replace(".vhdx","")
+} else {
+    Write-Host "Error: env:IMAGE is NULL"
+    exit 100
+}
+
+New-Variable -Name vmArray -Value @()
+if ($dual){
+    Set-Variable -Name testType -Value "downstream" -Option constant -Scope Script
+    ( Get-Variable -Name "vmArray" ).Value += "${tmp}-${nvr}-${testType}-A"
+    ( Get-Variable -Name "vmArray" ).Value += "${tmp}-${nvr}-${testType}-B"
+    Set-Variable -Name suite -Value "debug" -Option constant -Scope Script
+} else {
+    Set-Variable -Name testType -Value "gating" -Option constant -Scope Script
+    ( Get-Variable -Name "vmArray" ).Value += "${tmp}-${nvr}-${testType}"
+    Set-Variable -Name suite -Value "debug" -Option constant -Scope Script
+}
+
+Set-Variable -Name hostFolder -Value "C:\${nvr}-${testType}" -Option constant -Scope Script
+
+
+$SecurePassword = $env:DOMAIN_PSW | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $env:DOMAIN_USR, $SecurePassword
+
+switch ($action)
+{
+    "add"
+    {
+        Write-Host "Info: Starting to remove $hostFolder"
+        Invoke-Command -computername $env:HOST_ID -Credential $cred -scriptblock {(Test-Path $args[0]) -and (Remove-Item $args[0] -Confirm:$false -recurse -Force)} -ArgumentList $hostFolder
+
+        Write-Host "Info: Starting to copy windows folder from JSlave to $hostFolder on $env:HOST_ID"
+        $Session = New-PSSession -ComputerName $env:HOST_ID -Credential $cred
+        Copy-Item ".\windows" -Destination $hostFolder -ToSession $Session -Recurse
+        Remove-PSSession -Session $Session
+
+        Write-Host "Info: Running HYPERV-Manager on $env:HOST_ID"
+        $ret = Invoke-Command -computername $env:HOST_ID -Credential $cred -scriptblock `
+        { `
+            Set-Location $args[0]; `
+            .\HYPERV-Test $args[1] -image $args[2] -gen2 $args[3] -omni_ip $args[4] -omni_port $args[5] -vmArray $args[6];`
+            return $LastExitCode
+        } `
+        -ArgumentList @($hostFolder, $action, $env:IMAGE, $gen2, ${env:OMNI_IP}, ${env:API_PORT}, $vmArray)
+        if ( $ret -ne 0 ) { 
+            write-host "Error: add vm failed"
+            exit 100
+        }
+    }
+    "run"
+    {
+        
+        Set-Variable -Name lisa_home -Value ".\lis\WS2012R2\lisa" -Option constant -Scope Script
+        #Set-Variable -Name vmNameB -Value "$env:name-$env:version-$env:release-$env:BUILD_ID-B" -Option constant -Scope Script
+        Copy-Item ".\windows\ssh\3rd_id_rsa.ppk" -Destination "${lisa_home}\ssh\"
+        Copy-Item ".\windows\bin\*" -Destination "${lisa_home}\bin\"
+        Copy-Item ".\windows\cases.xml" -Destination "${lisa_home}\xml\"
+
+        Write-Host "Info: Starting to copy LISA from JSlave to $hostFolder on $env:HOST_ID"
+        $Session = New-PSSession -ComputerName $env:HOST_ID -Credential $cred
+        Copy-Item $lisa_home -Destination $hostFolder -ToSession $Session -Recurse
+        Remove-PSSession -Session $Session
+
+        Write-Host "Info: Running $suite test cases on $env:HOST_ID"
+        Invoke-command -computername $env:HOST_ID -Credential $cred -scriptblock `
+        { `
+            Set-Location $args[0]; `
+            .\lisa run .\xml\cases.xml -vmName $args[1] -hvServer $args[2] -sshKey 3rd_id_rsa.ppk -suite $args[3] -os Linux -dbgLevel 10 -testParams "VM2NAME=${args[4]};SSH_PRIVATE_KEY=id_rsa_private" `
+        } `
+        -ArgumentList "${hostFolder}\lisa", $vmArray[0], $env:HOST_ID, $suite, $vmArray[1]
+
+        Write-Host "Info: Copying result back to JSlave"
+        $resultDir = "${hostFolder}\lisa\TestResults"
+        $Session = New-PSSession -ComputerName $env:HOST_ID -Credential $cred
+        Copy-Item $resultDir -Destination ".\" -FromSession $Session -Recurse -Confirm:$false
+        Remove-PSSession -Session $Session
+    
+    }
+    "del"
+    {
+        Write-Host "Info: Removing VM(s) on $env:HOST_ID"
+        Invoke-Command -computername $env:HOST_ID -Credential $cred -scriptblock `
+        { `
+            Set-Location $args[0]; `
+            .\HYPERV-Test $args[1] -image $args[2] -gen2 $args[3] -omni_ip $args[4] -omni_port $args[5] -vmArray $args[6] `
+        } `
+        -ArgumentList $hostFolder, $action, $env:IMAGE, $gen2, ${env:OMNI_IP}， ${env:API_PORT}, $vmArray
+
+        Write-Host "Info: Removing $hostFolder on $env:HOST_ID"
+        Invoke-Command -ComputerName $env:HOST_ID -Credential $cred -ScriptBlock `
+        { `
+            Remove-Item $args[0] -Force -Confirm:$false -Recurse `
+        } `
+        -ArgumentList $hostFolder
+    }
+    "put"
+    {
+        Write-Host "Info: Uploading Report.xml to $env:OMNI_IP"
+        Copy-Item ".\TestResults\*\Report*.xml" -Destination ".\report-${env:HOST_ID}${smoke}-${env:owner}-${env:id}.xml"
+        Write-Output y | windows\bin\pscp -P $env:API_PORT -l $env:OMNI_USER -i windows\ssh\3rd_id_rsa.ppk ".\report-${env:HOST_ID}${smoke}-${env:owner}-${env:id}.xml" ${env:OMNI_IP}:
+        Write-Host "Info: Action PUT exit code: $global:LastExitCode"
+    }
+    "start"
+    {
+        if ($esxi)
+        {
+            Set-Location ".\windows"
+            .\ESX-Manager $action -dual $dual -name $env:name -version $env:version -release $env:release -image $env:IMAGE -buildID $env:BUILD_ID -gen2 $gen2 -omni_ip $env:OMNI_IP -omni_port $env:API_PORT -omni_user $env:OMNI_USER -hostID $env:HOST_ID -vsphereServer $env:ENVVISIPADDR -vsphereProtocol $env:ENVVISPROTOCOL -credential $cred
+            Write-Host "Info: Action ADD exit code: $global:LastExitCode"
+        }
+        else
+        {
+            Write-Host "Info: Starting to remove $hostFolder"
+            Invoke-Command -computername $env:HOST_ID -Credential $cred -scriptblock {(Test-Path $args[0]) -and (Remove-Item $args[0] -Confirm:$false -recurse -Force)} -ArgumentList $hostFolder
+
+            Write-Host "Info: Starting to copy windows folder from JSlave to $hostFolder on $env:HOST_ID"
+            $Session = New-PSSession -ComputerName $env:HOST_ID -Credential $cred
+            Copy-Item ".\windows" -Destination $hostFolder -ToSession $Session -Recurse
+            Remove-PSSession -Session $Session
+
+            Write-Host "Info: Running HYPERV-Manager on $env:HOST_ID"
+            Invoke-Command -computername $env:HOST_ID -Credential $cred -scriptblock `
+            { `
+                Set-Location $args[0]; `
+                .\HYPERV-Manager $args[1] -dual $args[2] -name $args[3] -version $args[4] -release $args[5] -image $args[6] -buildID $args[7] -gen2 $args[8] -omni_ip $args[9] -omni_port $args[10] -omni_user $args[11] -compose_ver $args[12]`
+            } `
+            -ArgumentList $hostFolder, $action, 0, "", "", "", $env:IMAGE, "", $gen2, $env:OMNI_IP, $env:API_PORT, $env:OMNI_USER, $env:ComposeVer
+        }
+    }
+}
---
+++ windows/HYPERV-Test.ps1
@@ -0,0 +1,233 @@
+param (
+    [CmdletBinding()]
+    [String] $action,
+    [String] $image,
+    [Bool] $gen2,
+    [String] $omni_ip,
+    [String] $omni_port,
+    [string[]]$vmArray,
+    [Int64] $cpuCount = 2,
+    [Int64] $memorySize = 2
+)
+
+Set-Variable -Name switchName -Value "External" -Option constant -Scope Script
+Set-Variable -Name arch -Value "x86_64" -Option constant -Scope Script
+Set-Variable -Name snapshotName -Value "ICABase" -Option constant -Scope Script
+Set-Variable -Name vmPath -Value "C:\downstream-vm\" -Option constant -Scope Script
+Set-Variable -Name imageURL -Value "http://${omni_ip}:${omni_port}" -Option constant -Scope Script
+
+Get-ChildItem .\Libraries -Recurse | Where-Object { $_.FullName.EndsWith(".psm1") } | ForEach-Object { Import-Module $_.FullName -Force}
+
+function GetImage([String]$vmPath, [String]$vmName, [String]$image, [String]$imageURL)
+{
+    Write-Host "Info: Downloading from ${imageURL}/${image} to $vmPath${vmName}.vhdx"
+    if (-not (Test-Path $vmPath))
+    {
+        New-Item -ItemType directory -Path $vmPath | Out-Null
+        Write-Host "Info: Create new vhdx directory $vmPath"
+    }
+    $start_time = Get-Date
+    (New-Object System.Net.WebClient).DownloadFile("$imageURL/${image}", "${vmPath}${vmName}.vhdx")
+    if ( -not $? ) {
+        write-host "Error: failed to download from ${imageURL}/${image} to $vmPath${vmName}.vhdx"
+        return $false
+    }
+    write-host "Info: Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s) for downloading"
+    if (Test-Path "${vmPath}${vmName}.vhdx") {
+        return $true
+    } 
+    return $false
+}
+
+function NewVMFromVHDX([String]$vmPath, [Switch]$gen2, [String]$switchName, [String]$vmName, [Int64]$cpuCount, [Int64]$mem)
+{
+    Write-Host "Info: Creating $vmName with $cpuCount CPU and ${mem}G memory."
+    # Convert GB to bytes because parameter -MemoryStartupByptes requires bytes
+    [Int64]$memory = 1GB * $mem
+
+    if ($gen2)
+    {
+        New-VM -Name "$vmName" -Generation 2 -BootDevice "VHD" -MemoryStartupBytes $memory -VHDPath $vmPath -SwitchName $switchName | Out-Null
+    }
+    else
+    {
+        New-VM -Name "$vmName" -BootDevice "IDE" -MemoryStartupBytes $memory -VHDPath $vmPath -SwitchName $switchName | Out-Null
+    }
+
+    if (-not $?)
+    {
+        Write-Host "New-VM $vmName failed"
+        # rm new created disk
+        If (Test-Path $vmPath){
+            Remove-Item $vmPath
+        }
+        return $false
+    }
+    Write-Host "Info: New-VM $vmName successfully"
+
+    # If gen 2 vm, set vmfirmware secure boot disabled
+    if ($gen2)
+    {
+        # disable secure boot
+        Set-VMFirmware $vmName -EnableSecureBoot Off
+
+        if (-not $?)
+        {
+            Write-Host "Info: Set-VMFirmware $vmName secureboot failed"
+            return $false
+        }
+
+        Write-Host "Info: Set-VMFirmware $vmName secureboot successfully"
+    }
+
+    # set processor to 2, default is 1
+    Set-VMProcessor -vmName $vmName -Count $cpuCount
+
+    if (! $?)
+    {
+        Write-Host "Error: Set-VMProcessor $vmName  to $cpuCount failed"
+        return $false
+    }
+
+    if ((Get-VMProcessor -vmName $vmName).Count -eq $cpuCount)
+    {
+        Write-Host "Info: Set-VMProcessor $vmName to $cpuCount"
+    }
+    return $true
+}
+
+function VMSetup([String]$vmPath, [String]$vmName, [String]$image, [String]$omni_ip, [String]$omni_port, [String]$omni_user, [Bool]$gen2, [String]$switchName, [Int64]$cpuCount, [Int64]$mem)
+{
+    GetImage -vmPath $vmPath -vmName $vmName -image $image -omni_ip $omni_ip -omni_port $omni_port -omni_user $omni_user
+    # remove vm if already exists same name VM already exists
+    VMRemove -vmName $vmName
+    # Create vm based on new vhdx file
+    if ($gen2)
+    {
+        NewVMFromVHDX -vmPath "${vmPath}${vmName}.vhdx" -gen2 -switchName $switchName -vmName $vmName -cpuCount $cpuCount -mem $mem
+    }
+    else
+    {
+        NewVMFromVHDX -vmPath "${vmPath}${vmName}.vhdx" -switchName $switchName -vmName $vmName -cpuCount $cpuCount -mem $mem
+    }
+    # Now Start the VM
+    Write-Host "Info: Starting VM $vmName."
+    $timeout = 300
+    Start-VM -Name $vmName
+    WaitForVMToStartKVP -vmName $vmName -timeout $timeout
+    $vmIP = GetIPv4ViaKVP $vmName
+
+    Write-Host "Info: Downloading $kernelName to VM $vmIP."
+    # Download kernel rpm from omni server and install
+    Write-Output y | plink -l root -i ssh\3rd_id_rsa.ppk $vmIP "exit 0"
+    Write-Output y | plink -l root -i ssh\3rd_id_rsa.ppk $vmIP "scp -o StrictHostKeyChecking=no -P $omni_port -i /root/.ssh/id_rsa_private data@${omni_ip}:kernel* . && yum install -y ./kernel* && reboot"
+
+    WaitForVMToStartKVP -vmName $vmName -timeout $timeout
+    $vmIP = GetIPv4ViaKVP $vmName
+
+    Write-Output y | plink -l root -i ssh\3rd_id_rsa.ppk $vmIP "exit 0"
+    $kernel = Write-Output y | plink -l root -i ssh\3rd_id_rsa.ppk $vmIP "uname -r"
+    Write-Host "Info: Get kernel version from VM ${vmName}: ${kernel}, expect ${kernelName} "
+    if ("${kernelName}.x86_64" -Match $kernel)
+    {
+        Write-Host "Info: Kernel version matched"
+        VMStop -vmName $vmName
+        NewCheckpoint -vmName $vmName -snapshotName $snapshotName
+    }
+    else
+    {
+        Write-host "ERROR: Unable get correct kernel version" -ErrorAction SilentlyContinue
+        VMStop -vmName $vmName
+        VMRemove -vmName $vmName -vmPath $vmPath
+    }
+}
+
+function VMStart([String]$vmPath, [String]$vmName, [String]$image, [Bool]$gen2, [String]$switchName, [Int64]$cpuCount, [Int64]$mem)
+{
+    # remove vm if already exists same name VM already exists
+    VMRemove -vmName $vmName
+
+    write-host "Info: vmName is $vmName"
+    $status = GetImage -vmPath $vmPath -vmName $vmName -image $image -imageURL $imageURL
+    if ($status -eq $false) {
+        return $null
+    }
+    
+    # Create vm based on new vhdx file
+    if ($gen2)
+    {
+        NewVMFromVHDX -vmPath "${vmPath}${vmName}.vhdx" -gen2 -switchName $switchName -vmName $vmName -cpuCount $cpuCount -mem $mem
+    }
+    else
+    {
+        NewVMFromVHDX -vmPath "${vmPath}${vmName}.vhdx" -switchName $switchName -vmName $vmName -cpuCount $cpuCount -mem $mem
+    }
+    # Now Start the VM
+    Write-Host "Info: Starting VM $vmName."
+    $timeout = 300
+    Start-VM -Name $vmName
+    WaitForVMToStartKVP -vmName $vmName -timeout $timeout
+    $vmIP = GetIPv4ViaKVP $vmName
+
+    if ($vmIP)
+    {
+        #Write-Output y | plink -l root -i ssh\3rd_id_rsa.ppk $vmIP "exit 0"
+        Write-Host "Info: Get $vmName IP = $vmIP"
+        return $vmIP
+    }
+    else
+    {
+        Write-host "ERROR: Unable get correct kernel version" -ErrorAction SilentlyContinue
+        VMStop -vmName $vmName
+        VMRemove -vmName $vmName -vmPath $vmPath
+    }
+    return $null
+}
+
+switch ($action)
+{
+    "add"
+    {
+        $hosts = ".\hosts"
+        if (Test-Path $hosts) {
+            Remove-Item $hosts -Confirm:$false -Force
+            New-Item -ItemType file -Path $hosts | Out-Null
+        }
+        foreach ($i in $vmArray) {
+            write-host "DEBUG: $i"
+            $ip = VMStart -vmPath $vmPath -vmName $i -image $image -gen2 $gen2 -switchName $switchName -cpuCount $cpuCount -mem $memorySize
+            if ( $ip[-1] ) {
+                Write-output $ip[-1] | Out-File -FilePath $hosts -Append -Encoding ASCII
+                write-host "INFO: get-content hosts"
+                get-content $hosts
+                write-host "-----------------"
+            } else { exit 100 }            
+        }
+        Write-Output y | pscp -l root -i ssh\3rd_id_rsa.ppk $hosts root@${omni_ip}:/root/
+        exit 0          
+    }
+    "del"
+    {
+        foreach ($i in $vmArray)
+        {
+            VMRemove -vmName $i
+        }
+    }
+    "start"
+    {
+        foreach ($i in $vmArray) {
+            write-host "DEBUG: $i"
+            exit 100
+        }
+        $ip1 = VMStart -vmPath $vmPath -vmName $vmName -image $image -gen2 $gen2 -switchName $switchName -cpuCount $cpuCount -mem $memorySize
+        if ( $ip1[-1] ) {
+            Write-output $ip1[-1] | Out-File -FilePath .\hosts -Encoding ASCII
+            write-host "INFO: get-content hosts"
+            get-content .\hosts
+            write-host "-----------------"
+            Write-Output y | pscp -l root -i ssh\3rd_id_rsa.ppk .\hosts root@${omni_ip}:/root/
+        } else {
+            return $false
+        }
+    }
+}
